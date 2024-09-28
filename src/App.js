568e27a69130b82333dd5ec9ea09d9d2c350418f
Bernardo Krause <107999271+BernardoKrause@users.noreply.github.com>
2024-09-28 11:51:31 -0300

    consertando conflitos do realizar reserva

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import Inicio from './pages/Inicio';
 import RealizarReserva from './pages/RealizarReserva';
+import CalendarPage from './pages/CalendarPage';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='realizar-reserva' element={<RealizarReserva />} />
+        <Route path='calendario' element={<CalendarPage />} />
       </Routes>
     </BrowserRouter>
   );
